50d8914c37f369c05ef40305b3c36a6831663de0
Dustin Petersen <d86petersen@gmail.com>
2022-03-02 09:14:16 -0800

    daily challenge

---
+++ day2/js/main.js
@@ -0,0 +1,14 @@
+const number = 3
+
+console.log(number)
+
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+console.log(bigInt)
+
+
+const text = "Supercalifragilisticexpialidocious"
+const length = text.length;
+
+console.log(length)
